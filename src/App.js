4f599796cc9024b8e9a0b1453481e50ceee50078
Aryan1982 <ap1982o3@gmail.com>
2022-12-31 11:43:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import VerificationPage from './components/VerificationPage'
-import Home from './components/Home'
-import About from './components/About'
-import {HashRouter as Router,Routes,Route} from "react-router-dom";
+import {useEffect,useState} from 'react'
+import QrReader from 'react-web-qr-reader';
 
 
 function App() {
+  const delay = 500;
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  const [result, setResult] = useState('No result');
+
+  const handleScan = (result) => {
+    if (result) {
+      setResult(result);
+    }
+  };
+
+  const handleError = (error) => {
+    console.log(error);
+  };
   return (
-      <div >
-        <VerificationPage/>
-      </div>
+    <>
+      <QrReader
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      /><br/><br/><br/><br/>
+      <p>{result}</p>
+    </>
 
      );
 }

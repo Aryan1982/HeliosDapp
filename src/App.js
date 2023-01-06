dc4f08aa73012c4e94cca51519147abf67862807
Aryan1982 <ap1982o3@gmail.com>
2023-01-06 22:11:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, {
-  useState
-} from "react";
-import QrReader from "react-web-qr-reader";
+// import React, {
+//   useState
+// } from "react";
+// import QrReader from "react-web-qr-reader";
 
-const Example = () => {
-  const delay = 500;
+// const Example = () => {
+//   const delay = 500;
 
-  const previewStyle = {
-    height: 240,
-    width: 320
-  };
+//   const previewStyle = {
+//     height: 240,
+//     width: 320
+//   };
 
-  const [result, setResult] = useState("Scan any QR");
+//   const [result, setResult] = useState("Scan any QR");
 
-  const handleScan = (result) => {
-    if (result) {
-      setResult(result.data);
-    }
-  };
+//   const handleScan = (result) => {
+//     if (result) {
+//       setResult(result.data);
+//     }
+//   };
 
-  const handleError = (error) => {
-    console.log(error);
-  };
+//   const handleError = (error) => {
+//     console.log(error);
+//   };
 
-  return (
-    <>
-      <QrReader
-        delay={delay}
-        style={previewStyle}
-        onError={handleError}
-        onScan={handleScan}
-      /><br/><br/><br/><br/>
-      <a href={result}>{result}</a>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <QrReader
+//         delay={delay}
+//         style={previewStyle}
+//         onError={handleError}
+//         onScan={handleScan}
+//       /><br/><br/><br/><br/>
+//       <a href={result}>{result}</a>
+//     </>
+//   );
+// };
 
-export default Example;
+// export default Example;
+import Hacknitr from './components/hacknitr'
+const App=()=>{
+  return(
+      <Hacknitr/>
+    );
+}
+
+export default App;
